bd3b9352ebea0f9a25b32f86d5170534f8650c8b
mcartage32 <mcartage33@gmail.com>
2025-01-30 22:39:24 -0500

    Se crean los servicios para las tareas y los usuarios

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,24 +1,24 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { NotesModule } from './notes/notes.module';
-import { TagsModule } from './tags/tags.module';
-import { Note } from './notes/note.entity';
-import { Tag } from './tags/tag.entity';
+import { UserModule } from './user/user.module';
+import { User } from './user/user.entity';
+import { Task } from './task/task.entity';
+import { TaskModule } from './task/task.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql', 
-      host: 'localhost', 
-      port: 3306, 
-      username: 'root', 
-      password: 'root', 
-      database: 'ensolvers_exercise', 
-      entities: [Note, Tag], 
-      synchronize: true, 
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'domina_test',
+      entities: [User, Task],
+      synchronize: true,
     }),
-    NotesModule,
-    TagsModule,
+    UserModule,
+    TaskModule,
   ],
 })
 export class AppModule {}
